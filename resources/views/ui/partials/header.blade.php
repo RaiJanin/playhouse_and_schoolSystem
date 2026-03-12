e39423b27a53a3437771b6c1a8774b407ab559d2
RaiJanin <raijaninoabrenica@gmail.com>
2026-03-12 11:56:52 +0800

    Change the logo with animated logo

--- resources/views/ui/partials/header.blade.php
+++ resources/views/ui/partials/header.blade.php
@@ -1,7 +1,8 @@
-<div class="sm:flex md:grid md:grid-cols-2 lg:grid-cols-3 py-[20px] px-4 bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-primary-mid-dark)] backdrop-blur-xs rounded-xl mb-[30px] shadow-sm z-20">
-    <div class="flex justify-center items-center gap-4 sm:gap-6"> 
-        <img src="/images/playhouse_bounce.gif" alt="MIMO Play Cafe Logo" class="sm:h-24 h-16 w-auto" onerror="this.onerror=null;this.src='/images/mimo-logo.png';" />
-
+<div class="sm:flex md:grid md:grid-cols-2 lg:grid-cols-3 bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-primary-mid-dark)] backdrop-blur rounded-xl mb-[30px] shadow-sm z-20">
+    <div class="w-[220] h-[80px] overflow-hidden flex items-center justify-center rounded-xl">
+        <img src="/images/mimo-logo.gif" alt="MIMO Play Cafe Logo" class="w-[200px] max-w-none object-cover object-center" onerror="this.onerror=null;this.src='/images/mimo-logo.png';" />
+    </div>
+    <div class="flex flex-col md:col-span-2 sm:flex-row justify-start items-center gap-4 sm:gap-6 p-4">
         <div class="flex flex-col items-start text-left">
             <h1 class="sm:text-3xl md:text-5xl text-2xl font-extrabold text-[var(--color-accent)] [text-shadow:2px_2px_4px_rgba(0,0,0,0.3)]">
                 Mimo Play Cafe
@@ -11,7 +12,7 @@
             </h1>
         </div>  
     </div>
-    <div class="lg:col-span-3 md:col-span-2">
+    <div class="lg:col-span-3 md:col-span-2 p-4">
         <div class="flex items-end justify-end">
             @if(Route::is('playhouse.checkout'))
                 <a class="text-sm sm:text-base text-[var(--color-accent)] font-semibold underline decoration-2 underline-offset-2 decoration-white" href="{{ route('playhouse.checkin.source')}}">Go to check in</a>
