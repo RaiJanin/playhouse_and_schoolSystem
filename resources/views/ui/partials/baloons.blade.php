0d650039658d3010c403887fbdebcbd229da3f1e
RaiJanin <raijaninoabrenica@gmail.com>
2026-04-30 16:56:22 +0800

    Changed the UI for landing page and centralized texts on header

---
+++ resources/views/ui/partials/baloons.blade.php
@@ -0,0 +1,3 @@
+<div class="absolute -top-6 left-1/4 text-4xl animate-float" style="animation-delay: 0s; color: #1abc9c">🎉</div>
+<div class="absolute -bottom-4 right-1/3 text-3xl animate-float" style="animation-delay: 0.5s; color: #0d9984">✨</div>
+<div class="absolute top-1/4 right-2 text-3xl animate-float" style="animation-delay: 1.2s; color: #1abc9c">🎈</div>
