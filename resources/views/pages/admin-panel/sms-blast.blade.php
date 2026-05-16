3b464c7a6742bdf1ee3d9bd3d2324971284dbf9b
RaiJanin <rai.janinoabrenica@gmail.com>
2026-05-16 12:22:01 +0800

    Sms Blast allow production access

--- resources/views/pages/admin-panel/sms-blast.blade.php
+++ resources/views/pages/admin-panel/sms-blast.blade.php
@@ -6,19 +6,15 @@
             </h2>
         </x-slot>
         <div class="flex-wrap gap-2 min-h-screen">
-            @if(app()->environment('production') && auth()->user()->name !== 'admin')
-                <x-in-development-placeholder />
-            @else
-                @if(request()->routeIs('sms_blast.index'))
-                    @include('ui.admin-panel.sms-blast.index')
-                @elseif(request()->routeIs('sms_blast.create'))
-                    @vite('resources/js/modules/admin-panel-create.js')
-                    @include('ui.admin-panel.sms-blast.create')
-                @elseif(request()->routeIs('sms_blast.show') && app()->environment('local'))
-                    @include('ui.admin-panel.sms-blast.details')
-                @elseif(request()->routeIs('sms_blast.edit') && app()->environment('local'))
-                    @include('ui.admin-panel.sms-blast.edit')
-                @endif
+            @if(request()->routeIs('sms_blast.index'))
+                @include('ui.admin-panel.sms-blast.index')
+            @elseif(request()->routeIs('sms_blast.create'))
+                @vite('resources/js/modules/admin-panel-create.js')
+                @include('ui.admin-panel.sms-blast.create')
+            @elseif(request()->routeIs('sms_blast.show'))
+                @include('ui.admin-panel.sms-blast.details')
+            @elseif(request()->routeIs('sms_blast.edit'))
+                @include('ui.admin-panel.sms-blast.edit')
             @endif
         </div>
     </div>
